333bac5cde32e626476f343e22a9538cdf09c8a2
Le Anh Toan <leanhtoan@vietbank.com.vn>
2024-06-11 15:08:29 +0700

    Config rabbitmq for  Direct Reply-to và Dead Letter Exchanges

---
+++ Ecommerce/Ecommerce.Infrastructure.Shared/Services/RabbitMqService.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace Ecommerce.Infrastructure.Shared.Services
+{
+    public class RabbitMqService : IRabbitMqService, IDisposable
+    {
+        private readonly IConnection _connection;
+        private readonly BlockingCollection<IModel> _channelPool;
+        private readonly int _maxChannels = 10; // Adjust based on your requirements
+
+        public RabbitMqService(IConnectionFactory connectionFactory)
+        {
+            _connection = connectionFactory.CreateConnection();
+            _channelPool = new BlockingCollection<IModel>(_maxChannels);
+            for (int i = 0; i < _maxChannels; i++)
+            {
+                _channelPool.Add(_connection.CreateModel());
+            }
+        }
+
+        public async Task PublishAsync(string queueName, string message)
+        {
+            var channel = _channelPool.Take();
+            try
+            {
+                // Ensure the queue is declared only once
+                lock (channel)
+                {
+                    if (channel.QueueDeclarePassive(queueName) == null)
+                    {
+                        channel.QueueDeclare(queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+                    }
+                }
+
+                var properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+                var body = Encoding.UTF8.GetBytes(message);
+                await Task.Run(() =>
+                {
+                    channel.BasicPublish(exchange: "", routingKey: queueName, basicProperties: properties, body: body);
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to publish message: {ex.Message}");
+                // Handle exception
+            }
+            finally
+            {
+                _channelPool.Add(channel);
+            }
+        }
+
+        public void Dispose()
+        {
+            foreach (var channel in _channelPool)
+            {
+                channel.Close();
+                channel.Dispose();
+            }
+            _connection.Close();
+            _connection.Dispose();
+        }
+
+    }
+
+    public interface IRabbitMqService
+    {
+        Task PublishAsync(string queueName, string message);
+    }
+}
--- docker-command.md
+++ docker-command.md
@@ -26,5 +26,7 @@ p, SuperAdmin, roleclaims, delete
 
 docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=sql@pa22w0rd" -p 1433:1433 -d --name=sqlserver mcr.microsoft.com/mssql/server:2022-preview-ubuntu-22.04
 
+docker run -d --name rabbitmq --network ecommerce -p 5672:5672 -p 15672:15672 -e RABBITMQ_DEFAULT_USER=ecommerce -e RABBITMQ_DEFAULT_PASS=123456789 -e RABBITMQ_MEMORY_HIGH_WATERMARK=0.7 -e RABBITMQ_DISK_FREE_LIMIT=500MB -e RABBITMQ_CONSUMER_TIMEOUT=300000 --cpus="2" --memory="2g" rabbitmq:3-management
+
 git rm --cached Ecommerce/Ecommerce.WebApp.Client/Ecommerce.WebApp.Client.esproj
 git rm --cached ./Ecommerce/Ecommerce.WebApp.Client/Ecommerce.WebApp.Client.esproj
